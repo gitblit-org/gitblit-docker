49a60086a15da2777adb6cfaa2f6c9e0cd75c286
Florian Zschocke <f.zschocke+git@gmail.com>
2025-05-24 18:03:04 +0200

    # This is a combination of 2 commits. # This is the 1st commit message:
    Add script to run container under dgoss
    
    This is just so that one doesn't have to remember the parameters and
    for less typing. It includes setting a parameter so that Gitblit can
    detect that it is run under dgoss.
    
    # The commit message #2 will be skipped:
    
    # fix

---
+++ dgoss_run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+IMAGE=$1
+
+if [[ -z "${IMAGE}" ]] ; then
+    echo "Usage: dgoss_run <docker image>"
+    echo " "
+    echo " Example: dgoss_run gitblit:snapshot"
+    exit 1
+fi
+
+export GOSS_WAIT_OPTS="-r 60s -s 10s"
+
+dgoss run  -e GITBLIT_GOSS_TEST=true -p 8080:8080  -p 8443:8443  ${IMAGE}
